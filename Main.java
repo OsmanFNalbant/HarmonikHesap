5780dc715e52ce7ed222be24d8c7e8afb062e8ba
OsmanFurkanNalbant <osmanfnalbant@outlook.com>
2022-09-11 05:52:32 +0300

    HarmonikHesap

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kaç elemanlı bir dizi olsun:");
+        int a = sc.nextInt();
+        double toplam=0;
+        double[] array = new double[a];
+        for(int i=0; i<a; i++)
+        {
+            array[i]=i+1;
+        }
+        for(int i=0; i<a; i++)
+        {
+            toplam += 1/array[i];
+        }
+        System.out.println("Sonuc : "+toplam);
+        sc.close();
+    }
+}
